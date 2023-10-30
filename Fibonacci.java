01966df9b56f7294e0e934b7e519ac57092b314e
bfellows71 <124815356+bfellows71@users.noreply.github.com>
2023-10-30 14:41:24 -0800

    Add files via upload
    There are two files: fibonacci and poorlywrittenfibonacci.
    
    The fibonacci.java will compile and run without errors. The poorlywrittenfibonacci, as the name implies, will not compile and run because of intentional errors. This is so we can use our code analysis and see if it will make changes it needs to run successfully.
    
    The following errors are:
    program will break if a non-integer input is given
    loop should be i <= n, but it starts printing one more term than needed
    missing semi-colon after input()

---
+++ Fibonacci.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of Fibonacci terms you want: ");
+        int n = input.nextInt();
+
+        int firstTerm = 0;
+        int secondTerm = 1;
+
+        System.out.print("Fibonacci Sequence: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(firstTerm);
+            if (i < n) {
+                System.out.print(", ");
+            }
+
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+
+        System.out.println("\nThe " + n + "th Fibonacci term is: " + firstTerm);
+
+        input.close();
+    }
+}
